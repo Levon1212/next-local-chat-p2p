04f33444c3b334dcf2556968554b2b0d6d8cd96d
Levon Sargsyan <sargsyanlevon12@gmail.com>
2025-08-14 16:59:38 +0200

    change url 2

--- lib/signal.ts
+++ lib/signal.ts
@@ -6,7 +6,7 @@ let socket: Socket | null = null;
 
 export function getSocket(): Socket {
     if (!socket) {
-        socket = io("local-chat-signal-production.up.railway.app", {
+        socket = io("https://local-chat-signal-production.up.railway.app/", {
             transports: ['websocket'],
         });
     }
